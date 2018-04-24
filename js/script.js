e2dc224876f246ddff7f5a212761b611234cd715
Pawel Bus <pawelbus.dev@outlook.com>
2018-04-24 12:23:14 +0200

    Add 'treeLeftSide' to script.js

--- js/script.js
+++ js/script.js
@@ -8,4 +8,29 @@ function treeRightSide(height) {
 	}
 }
 
+function treeLeftSide(height) {
+	space = height - 1;
+
+	for (var i = 0; i <= height; i++) {
+		var row = '';
+		
+		for (var j = 0; j <= space; j++) {
+			row += ' ';
+		}
+		
+		for (var k = 0; k <= i; k++) {
+			row += '*';
+		}
+
+		space--;
+		console.log(row);
+	}
+}
+
 treeRightSide(5);
+
+console.log();
+console.log('-----------------------------');
+console.log();
+
+treeLeftSide(5);
